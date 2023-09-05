d077cc97be066c575c903c08e398c90dbec469e4
Juules32 <benjaminbyvej@gmail.com>
2023-09-05 23:37:23 +0200

    improved eval outputs

--- engine/board.cpp
+++ engine/board.cpp
@@ -107,7 +107,6 @@ void board::parse_fen(std::string fen)
 void board::search_position(int depth)
 {
     stop_calculating = false;
-    found_checkmate = false;
 
     // Resets helper arrays
     memset(killer_moves, 0, sizeof(killer_moves));
@@ -119,14 +118,18 @@ void board::search_position(int depth)
     int beta = 50000;
     int candidate_pv_table_copy[246][246];
     
+    U64 bitboards_copy[12], occupancies_copy[3];                              
+    int side_copy, en_passant_copy, castle_copy;                              
+    memcpy(bitboards_copy, state::bitboards, size_of_bitboards);       
+    memcpy(occupancies_copy, state::occupancies, size_of_occupancies); 
+    side_copy = state::side, en_passant_copy = state::en_passant, castle_copy = state::castle;
+
     for (int current_depth = 1; current_depth <= depth; current_depth++)
     {
-        
-        if(stop_calculating || found_checkmate) break;
+        if(stop_calculating) break;
 
         memcpy(&candidate_pv_table_copy, &pv_table, sizeof(pv_table));
 
-        current_eval = 0;
         nodes = 0;
 
         int score = board::negamax(alpha, beta, current_depth);
@@ -134,7 +137,17 @@ void board::search_position(int depth)
         cout << endl;
         if(!stop_calculating) {
             cout << "Found best move at depth " << current_depth << " looking through " << nodes << " nodes" << endl;
+            
+            for (int i = 0; i < pv_length[0]; i++)
+            {
+                board::make_move(pv_table[0][i]);
+            }
+            int current_eval = quiescence(alpha, beta);
             cout << "Evaluation: " << format::eval(current_eval) << endl;
+
+            memcpy(state::bitboards, bitboards_copy, size_of_bitboards);       
+            memcpy(state::occupancies, occupancies_copy, size_of_occupancies); 
+            state::side = side_copy, state::en_passant = en_passant_copy, state::castle = castle_copy;
         }
         else {
             cout << "Interrupted by time at depth " << current_depth << " looking through " << nodes << " nodes" << endl;
